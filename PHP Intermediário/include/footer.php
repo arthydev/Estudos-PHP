295e8006ccb367547adb6828f41e544320832a0f
Arthur Catelani <arthurhccomercial@gmail.com>
2022-05-30 19:33:00 -0300

    Include

---
+++ PHP Intermediário/include/footer.php
@@ -0,0 +1,3 @@
+<div style="width: 100%; text-align: center;">
+    <h3>Footer do site</h3>
+</div>
---
+++ PHP Intermediário/include/header.php
@@ -0,0 +1,3 @@
+<div style="width: 100%; text-align: center;">
+    <h2>Header do site</h2>
+</div>
