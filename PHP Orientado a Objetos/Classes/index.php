6b0212c7381a8392fb9267631bb08b665c5d1f04
Arthur Catelani <arthurhccomercial@gmail.com>
2022-06-01 20:19:56 -0300

    Class

---
+++ PHP Orientado a Objetos/Classes/index.php
@@ -0,0 +1,28 @@
+<?php 
+
+    # Criando uma classe
+    class Pessoa{
+
+        private $nome = 'Jore';
+        public $idade = 25;
+        private $cidade = 'BH';
+
+        public function envelhecer(){
+            echo '<p>Estou envelhecendo</p>';
+        }
+
+        private function mudando(){
+            echo '<p>Estou me mudando</p>';
+        }
+
+    }
+
+    # Instanciando uma classe
+
+    $pessoa = new Pessoa;
+
+    $pessoa -> envelhecer(); # Acessando o metedodo, executa a função pois ela é publica
+
+    $pessoa -> mudando();   # Acessando o metedodo, não executa a função pois ela é privada
+
+?>
