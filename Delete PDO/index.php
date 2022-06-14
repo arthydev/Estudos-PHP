3843b9e9b16c69d2c2902b7b581fc18f7a6488f4
Arthur Catelani <arthurhccomercial@gmail.com>
2022-06-14 19:55:15 -0300

    DELETE

---
+++ Delete PDO/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+$HOST = 'localhost'; # Endereço de hospedagem do Banco de Dados
+$DBNAME = 'php_banco_de_dados'; # Nome do Banco de Dados
+$DBUSER = 'root'; # Usuário de acesso
+$DBPASS = ''; # Senha de Acesso
+
+$pdo /*classe nativa para gerenciar DB com php*/ = new PDO('mysql:host='.$HOST.';dbname='.$DBNAME,$DBUSER,$DBPASS);
+
+    $sql = $pdo->prepare("DELETE FROM `clientes` WHERE id=?");
+
+    $id = 1;
+
+    if($sql->execute(array($id))){
+        echo 'Cliente Deletado Com Sucesso!';
+    };
+
+?>
